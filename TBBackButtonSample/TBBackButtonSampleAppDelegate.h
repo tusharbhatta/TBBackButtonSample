db4db0f588290719eb57a80e94dc1346e30658f0
Tushar <tushar@crri.co.in>
2011-10-14 23:57:52 +0530

    Initial Commit

---
+++ TBBackButtonSample/TBBackButtonSampleAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  TBBackButtonSampleAppDelegate.h
+//  TBBackButtonSample
+//
+//  Created by Tushar on 10/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TBBackButtonSampleViewController;
+
+@interface TBBackButtonSampleAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet TBBackButtonSampleViewController *viewController;
+
+@end
---
+++ TBBackButtonSample/TBBackButtonSampleViewController.h
@@ -0,0 +1,15 @@
+//
+//  TBBackButtonSampleViewController.h
+//  TBBackButtonSample
+//
+//  Created by Tushar on 10/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBBackButtonSampleViewController : UIViewController {
+    
+}
+
+@end
